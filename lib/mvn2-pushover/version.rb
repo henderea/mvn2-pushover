96bb46f7071547f6e8c5122ac76ba2be6335c5d4
Eric Henderson <henderea@gmail.com>
2016-02-06 11:12:10 -0500

    enable screen lock checking

--- lib/mvn2-pushover/version.rb
+++ lib/mvn2-pushover/version.rb
@@ -1,3 +1,3 @@
 module Mvn2Pushover
-  VERSION = '1.0.0'
+  VERSION = '1.0.2'
 end
--- lib/mvn2/plugin/pushover.plugin.rb
+++ lib/mvn2/plugin/pushover.plugin.rb
@@ -1,16 +1,34 @@
 require 'everyday-plugins'
 include EverydayPlugins
 require 'pushover'
-class GrowlPlugin
+class PushoverPlugin
   extend Plugin
   extend PluginType
 
   register :option_with_param, sym: :pushover_token, names: %w(--pushover-token), desc: 'use pushover.net with the specified user token'
+  register :option, sym: :pushover_lock_check, names: %w(--pushover-lock-check), desc: 'skip the pushover notification when the screen is unlocked'
 
   register(:notification, order: 20) { |options, _, _, message_text|
     if options[:pushover_token]
       path = File.basename(Dir.getwd)
-      Pushover.notification(message: message_text, title: path, user: options[:pushover_token], token: 'aV6NxV11TLQJyVfmRvk1Rrv3xoWnGy')
+      if options[:pushover_lock_check]
+        rval = `python -c 'import sys,Quartz; d=Quartz.CGSessionCopyCurrentDictionary(); print d.get("CGSSessionScreenIsLocked", 0)'`.chomp.to_s
+        if %w(0 1).include?(rval)
+          unlocked = rval == '0'
+        else
+          unlocked = false
+          puts <<-EOS
+You can enable the ability to skip the pushover message when your Mac is unlocked by running
+$ pip install pyobjc-framework-Quartz
+to install the Python libraries needed to check the screen lock status.
+
+This is a Mac only feature
+EOS
+        end
+      else
+        unlocked = false
+      end
+      Pushover.notification(message: message_text, title: path, user: options[:pushover_token], token: 'aV6NxV11TLQJyVfmRvk1Rrv3xoWnGy') unless unlocked
     end
   }
 end
